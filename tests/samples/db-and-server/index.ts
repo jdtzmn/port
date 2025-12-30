a273019129aca20c57027f71632fba48363ae021
Jacob Daitzman <jdtzmn@gmail.com>
2025-12-29 21:43:32 -0500

    Use a random number in the db-and-server sample
    Helps ensure the services truly are separate

--- tests/samples/db-and-server/index.ts
+++ tests/samples/db-and-server/index.ts
@@ -3,8 +3,12 @@ import type { Context } from 'hono'
 
 const app = new Hono()
 
+const randomNumber = (() => Math.floor(Math.random() * 100))()
+
 app.get('/', (c: Context) => {
-  return c.text('Hello from Hono! The server is running.')
+  return c.text(
+    'Hello from Hono! The server is running. Random number between 0 and 99: ' + randomNumber
+  )
 })
 
 const port = 3000
