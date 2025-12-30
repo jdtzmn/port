5264af6d7ad67fbed4df11ee318fe311e90dd6e5
Jacob Daitzman <jdtzmn@gmail.com>
2025-12-30 01:04:42 -0500

    Check that the URL is reachable within the `up` command

--- src/commands/up.test.ts
+++ src/commands/up.test.ts
@@ -1,35 +1,48 @@
+import { basename } from 'node:path'
 import { waitFor } from 'cli-testing-library'
 import { describe, test, expect } from 'vitest'
 import { prepareSample, renderCLI } from '../../tests/utils'
-import { execAsync } from '../lib/exec.ts'
+import { sanitizeFolderName } from '../lib/sanitize'
+
+const SAMPLES_TIMEOUT = 20_000
 
 describe('samples start', () => {
-  test('should start the db-and-server sample', async () => {
-    const sample = await prepareSample('db-and-server')
+  test(
+    'start the db-and-server sample',
+    async () => {
+      const sample = await prepareSample('db-and-server', {
+        initWithConfig: true,
+      })
 
-    // Set up git and init the project
-    await execAsync('git init', {
-      cwd: sample.dir,
-    })
-    const initInstance = await renderCLI(['init'], sample.dir)
-    await waitFor(() => expect(initInstance.hasExit()).toMatchObject({ exitCode: 0 }))
+      const { findByText } = await renderCLI(['up'], sample.dir)
 
-    const { findByText } = await renderCLI(['up'], sample.dir)
+      const instance = await findByText(
+        'Traefik dashboard:',
+        {},
+        {
+          timeout: SAMPLES_TIMEOUT,
+        }
+      )
+      expect(instance).toBeInTheConsole()
 
-    const instance = await findByText(
-      'Traefik dashboard:',
-      {},
-      {
-        timeout: 19000,
-      }
-    )
-    expect(instance).toBeInTheConsole()
+      // Confirm that the domain is reachable
+      const sampleDir = sanitizeFolderName(basename(sample.dir))
+      const url = `http://${sampleDir}.port:3000`
+      const res = await fetch(url)
+      setInterval(() => {
+        // Keep checking until the status is 200
+        if (res.status === 200) {
+          clearInterval()
+        }
+      }, 1000)
 
-    // End the sample (use -y to skip Traefik confirmation prompt)
-    const downInstance = await renderCLI(['down', '-y'], sample.dir)
-    await waitFor(() => expect(downInstance.hasExit()).toMatchObject({ exitCode: 0 }), {
-      timeout: 19000,
-    })
-    sample.cleanup()
-  }, 20000)
+      // End the sample (use -y to skip Traefik confirmation prompt)
+      const downInstance = await renderCLI(['down', '-y'], sample.dir)
+      await waitFor(() => expect(downInstance.hasExit()).toMatchObject({ exitCode: 0 }), {
+        timeout: SAMPLES_TIMEOUT,
+      })
+      sample.cleanup()
+    },
+    SAMPLES_TIMEOUT + 1000
+  )
 })
