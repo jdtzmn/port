a9d53bcfd4bff4b042a1b84b551b0575e39d0a61
Jacob Daitzman <jdtzmn@gmail.com>
2026-01-21 23:46:44 -0500

    Exclude the `.port` directory from vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
     globals: true,
     environment: 'node',
     setupFiles: ['./tests/setup.ts'],
+    exclude: ['**/node_modules/**', '**/.port/**'],
   },
   resolve: {
     alias: {
